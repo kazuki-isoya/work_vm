b068d527234e6b40635d66bc59838bfd62d29de1
moru more <moremoru.world21@gmail.com>
2020-03-23 15:52:41 +0900

    磯谷コードをコミット

--- vm_class_test.rb
+++ vm_class_test.rb
@@ -1,7 +1,6 @@
 # 実行例
 # irb
-# require '/Users/shibatadaiki/work_shiba/full_stack/sample.rb'
-# （↑のパスは、自動販売機ファイルが入っているパスを指定する）
+# require /Users/morumore/Desktop/ワーク/work_vm/vm_class_test_rb
 # 初期設定（自動販売機インスタンスを作成して、vmという変数に代入する）
 # vm = VendingMachine.new
 # 作成した自動販売機に100円を入れる
@@ -18,45 +17,47 @@ class VendingMachine
   # （自動販売機に投入された金額をインスタンス変数の @slot_money に代入する）
   def initialize
     # 最初の自動販売機に入っている金額は0円
+    # 売上の初期値は0円
     @slot_money = 0
+    @sale = 0
   end
-  # 投入金額の総計を取得できる。
+
+  # 投入金額の合計を取得できる。
   def current_slot_money
-    # 自動販売機に入っているお金を表示する
-    # puts "合計投入金額#{@slot_money}円"
     return @slot_money
   end
   # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
   # 投入は複数回できる。
   def slot_money(money)
     if MONEY.include?(money) == true
-      # puts "#{money}を投入"
-      @slot_money = @slot_money + money
-    else
-      # puts "無効なお金です"
-      # puts "投入した#{money}円をそのまま返却"
-      return @slot_money
+      @slot_money += money
+    end
+  end
+
+# Drinkの購入処理、投入金額の合計から購入するドリンクの値段1本分が引かれ、ドリンクの数が1本減る。
+  def buy(name, price, count)
+    if @slot_money >= price
+      @slot_money -= price
+      count -= 1
     end
   end
   # 払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。
   def return_money
     # 返すお金の金額を表示する
-    # puts "#{@slot_money}円を返却します"
     # 自動販売機に入っているお金を0円に戻す
     @slot_money = 0
-    return @slot_money
   end
 end
 
 # step2:ジュースの管理
-#coke_stock = Stock.new("coke", 5)
+#coke_stock = Stock.new("coke", 5, 120)
 class Drink
   attr_reader :drink, :price
   attr_accessor :count
-  def initialize(drink, count, price)
-    @drink = drink
-    @count = count
+  def initialize(drink, price, count)
+    @name = name
     @price = price
+    @count = count
   end
 end
 # step2ここまで
@@ -64,14 +65,14 @@ end
 
 # step3:購入処理
 # purchase = Purchase.new
-class Purchase
+# class Purchase
 
-  def initialize
-    @sales = 0
+  # def initialize
+  #   @sales = 0
     # @drink_stock = stock.drink_stock
     # @coke_count = stock.coke_count
     # @coke_price = stock.coke_price
-  end
+  # end
 
 
   # お金を入れる処理
@@ -83,14 +84,14 @@ class Purchase
   # p vm.current_slot_money
 
   #購入処理
-  def buy(drink, price)
-    if @current_slot_money >= price
-      @sales += price
-      @current_slot_money -= price
-    end
-  end
-
-end
+#   def buy(drink, price)
+#     if @current_slot_money >= price
+#       @sales += price
+#       @current_slot_money -= price
+#     end
+#   end
+#
+# end
 # step3ここまで
 
 
